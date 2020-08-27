5cb9b5428242b9ce1544180b4e12a7e74e1be48f
lavanyasrivalli <38532403+lavanyasrivalli@users.noreply.github.com>
2020-08-27 03:59:32 -0400

    Revert "url updated"
    This reverts commit 4fb37a21f9daafd5b6ba398b494bb78b556f68a4.

--- src/graphQLClient.tsx
+++ src/graphQLClient.tsx
@@ -3,7 +3,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const networkInterface = createNetworkInterface({
-  uri: 'http://100.81.5.26:9097/graphql'
+  uri: 'http://localhost:9097/graphql'
 });
 export const gQLClient = new ApolloClient({
   networkInterface: networkInterface
