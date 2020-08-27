4076bf65bab62309ea7c079281ae9f63835c2a73
mounika <35794949+mounika-moparthy@users.noreply.github.com>
2020-08-27 04:19:18 -0400

    Update graphQLClient.tsx

--- src/graphQLClient.tsx
+++ src/graphQLClient.tsx
@@ -3,7 +3,9 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const networkInterface = createNetworkInterface({
-  uri: 'http://localhost:9097/graphql'
+  // uri: 'http://localhost:9097/graphql'
+    uri: 'http://100.81.5.26:9097/graphql'
+
 });
 export const gQLClient = new ApolloClient({
   networkInterface: networkInterface
